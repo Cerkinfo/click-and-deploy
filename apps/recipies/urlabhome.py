1cf7123e79ef59c09718b14307fda277956db5a3
Nikita Marchant <nikita.marchant@gmail.com>
2015-04-09 10:16:00 +0200

    Run pip install and generate new graphs

--- apps/recipies/urlabhome.py
+++ apps/recipies/urlabhome.py
@@ -3,13 +3,18 @@ from fabric import state
 
 DISTANT_PATH = 'homepage'
 
-def pull():
+def pull_update():
     with cd(DISTANT_PATH):
         run('git pull')
+        run('/home/homepage/homepage/ve/bin/pip install -r requirements.txt')
 
 def restart_services():
     run('sudo supervisorctl restart homepage')
 
+def gen_graphs():
+    run('/home/homepage/homepage/openings_cron_script.sh')
+
 def deploy():
     pull()
     restart_services()
+    gen_graphs()
