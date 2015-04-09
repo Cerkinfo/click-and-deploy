baa2fec1059a44e99291cc1ec0808ced24714b5f
Nikita Marchant <nikita.marchant@gmail.com>
2015-04-09 10:25:02 +0200

    pep8

--- apps/recipies/urlabhome.py
+++ apps/recipies/urlabhome.py
@@ -3,16 +3,20 @@ from fabric import state
 
 DISTANT_PATH = 'homepage'
 
+
 def pull_update():
     with cd(DISTANT_PATH):
         run('git pull')
         run('/home/homepage/homepage/ve/bin/pip install -r requirements.txt')
 
+
 def restart_services():
     run('sudo supervisorctl restart homepage')
 
+
 def gen_graphs():
     run('/home/homepage/homepage/openings_cron_script.sh')
+
 
 def deploy():
     pull_update()
